c4e47d3904d7de1c46dfb407a7232787a6aa3f0d
Milad Khajavi <khajavi@gmail.com>
2017-04-14 01:55:46 +0430

    initial commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/gregjones/httpcache"
+	"github.com/gregjones/httpcache/diskcache"
+	"github.com/peterbourgon/diskv"
+
+	"willnorris.com/go/imageproxy"
+)
+
+func main() {
+
+
+	// Set server address
+	addr :=  os.Getenv("PORT")
+
+	//if addr == "" {
+	//	log.Fatal("Noo address provided for the imageproxy")
+	//}
+
+	// Set cache
+	var cache httpcache.Cache
+	d := diskv.New(diskv.Options{
+		BasePath:     "/tmp/imageproxy",
+		CacheSizeMax: 500 * 1024 * 1024,
+	})
+	cache = diskcache.NewWithDiskv(d)
+
+	// Create proxy
+	p := imageproxy.NewProxy(nil, cache)
+
+	// Create whitelist
+	if os.Getenv("WHITELIST") != "" {
+		p.Whitelist = strings.Split(os.Getenv("WHITELIST"), ",")
+	}
+
+	// Create baseurl
+	if os.Getenv("BASEURL") != "" {
+		var err error
+		p.DefaultBaseURL, err = url.Parse(os.Getenv("BASEURL"))
+		if err != nil {
+			log.Fatalf("error parsing baseURL: %v", err)
+		}
+	}
+
+	p.ScaleUp = true
+
+	server := &http.Server{
+		Addr:    ":" + addr,
+		Handler: p,
+	}
+
+	fmt.Printf("imageproxy listening on " + addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
